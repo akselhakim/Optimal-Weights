9dffc5d3f05075c0c984eef07d325ae3c9b32a05
akselhakim <hakimaksel1@gmail.com>
2020-11-19 00:41:58 +0100

    Multiple stocks

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,39 @@ import {apiCall} from './apiCall'
 
 //const url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&outputsize=full&apikey=KA4TB78PB0MOW2AG";
 document.getElementById("click").onclick = start;
+document.getElementById("add").onclick = add;
+
+var ul = document.getElementById("stocks").children;
+(<HTMLButtonElement>document.getElementById("click")).disabled = true;
 
 async function start(){ 
+  var returnArrays = [];
+  for(var i = 0; i < ul.length; i++){
+    var symbol = ul[i].innerHTML;
+    var returns : number[] = await apiCall.makeApiCall(symbol);
+    document.getElementById("res").innerHTML = returns.toString();
+    returnArrays[i] = returns;
+  }
+
+  analytics(returnArrays)
+}
+
+function add(){
+  var ul = document.getElementById("stocks");
+  
+  var listElement = document.createElement("li");
   var symbol = (<HTMLInputElement>document.getElementById("symbol")).value;
-  var jsonRes = await apiCall.makeApiCall(symbol);
-  document.getElementById("res").innerHTML = jsonRes.toString();
+
+  listElement.appendChild(document.createTextNode(symbol));
+  ul.append(listElement);
+
+  if(document.getElementById("stocks").children.length > 0){
+    (<HTMLButtonElement>document.getElementById("click")).disabled = false;
+  }
+}
+
+function analytics(returnArrays : number[][]){
+
 }
 //start();
 
